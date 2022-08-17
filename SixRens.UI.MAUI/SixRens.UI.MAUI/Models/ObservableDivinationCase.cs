7df66441b54054e954f927cb5053ef02f7d1e469
yueyinqiu <lujiaqi2003@126.com>
2022-08-17 15:25:03 +0800

    csth

---
+++ SixRens.UI.MAUI/SixRens.UI.MAUI/Models/ObservableDivinationCase.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using SixRens.Core.占例存取;
+using SixRens.Core.壬式生成;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SixRens.UI.MAUI.Models
+{
+    public sealed partial class ObservableDivinationCase : ObservableObject
+    {
+        public ObservableDivinationCase(占例 dCase, bool makeCopy = false)
+        {
+            this.BindingCase = makeCopy ? 占例.反序列化(dCase.序列化()) : dCase;
+        }
+
+        public 占例 BindingCase { get; }
+
+        public string Text
+        {
+            get => BindingCase.断语;
+            set
+            {
+                this.OnPropertyChanging();
+                BindingCase.断语 = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public DateTime? Time => BindingCase.西历时间;
+
+        public 壬式 Plate => BindingCase.壬式;
+    }
+}
